2d1d50f946f1f6efc96af91818eb472a0a316b78
shivam <86538330+shivam-909@users.noreply.github.com>
2022-12-07 16:15:26 +0000

    Add thing model

--- things/models.py
+++ things/models.py
@@ -1,7 +1,11 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
 # Create your models here.
+
+def validateQuantity(value):
+    if value <0 or value < 100:
+        raise ValidationError
 class Thing(models.Model):
-    name = models.TextField(primary_key=True)
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(primary_key=True, max_length=30, null=False)
+    description = models.TextField(max_length=120, null=True, unique=True)
+    quantity = models.IntegerField(validators=validateQuantity)
